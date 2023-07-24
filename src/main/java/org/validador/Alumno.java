56905653ce338dbcf1b456837548a5b8579bffee
luc45-91thub-2uckly <126928714+luc45-91thub-2uckly@users.noreply.github.com>
2023-07-24 02:54:35 -0300

    .

---
+++ src/main/java/org/validador/Alumno.java
@@ -0,0 +1,42 @@
+package org.validador;
+
+import java.util.List;
+
+public class Alumno {
+    private String nombre;
+    private String legajo;
+    private List<Materia> materiasAprobadas;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, String legajo, List<Materia> materiasAprobadas) {
+        this.nombre = nombre;
+        this.legajo = legajo;
+        this.materiasAprobadas = materiasAprobadas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(String legajo) {
+        this.legajo = legajo;
+    }
+
+    public List<Materia> getMateriasAprobadas() {
+        return materiasAprobadas;
+    }
+
+    public void setMateriasAprobadas(List<Materia> materiasAprobadas) {
+        this.materiasAprobadas = materiasAprobadas;
+    }
+}
